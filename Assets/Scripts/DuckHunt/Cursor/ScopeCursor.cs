38ece1f2015e0eff7fd377cb4d52e70612a2de2d
ArcKontyR <kontyrtime@mail.ru>
2026-05-16 20:01:26 +0500

    added shoot sound

--- Assets/Scripts/DuckHunt/Cursor/ScopeCursor.cs
+++ Assets/Scripts/DuckHunt/Cursor/ScopeCursor.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(RectTransform))]
-public class ScopeCursor : MonoBehaviour
+public class ScopeCursor : MonoBehaviour, IPointerClickHandler
 {
     private RectTransform rectTransform;
     private ScopeCanvas scopeCanvas;
+
+    [SerializeField] private AudioSource shootSFX;
 
     void Awake()
     {
@@ -27,5 +29,10 @@ public class ScopeCursor : MonoBehaviour
     void Move(PointerEventData eventData)
     {
         rectTransform.position = eventData.position;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        shootSFX.Play();
     }
 }
